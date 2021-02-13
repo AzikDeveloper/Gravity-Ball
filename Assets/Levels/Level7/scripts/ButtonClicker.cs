eec171e421a70ec3ae1f5d2a3239181540b0df6e
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-02-13 13:27:54 +0500

    level7 completed

--- Assets/Levels/Level7/scripts/ButtonClicker.cs
+++ Assets/Levels/Level7/scripts/ButtonClicker.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class ButtonClicker : MonoBehaviour
 {
     public Animator animator;
+    public ParticleSystem enemyExplotionEffect;
+
+    public GameObject enem1;
+    public GameObject enem2;
+
+
+ 
+    bool isClicked = false;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "ExplodeButton")
+        if (!isClicked && collision.collider.tag == "ExplodeButton")
         {
             animator.Play("buttonAnim");
+            Invoke("explodeEnemy", 0.5f);
+            isClicked = true;
+            Debug.Log("boom");
         }
     }
+
+    void explodeEnemy()
+    {
+        ParticleSystem exp1 = Instantiate(enemyExplotionEffect, enem1.transform.position, enem1.transform.rotation);
+        ParticleSystem exp2 = Instantiate(enemyExplotionEffect, enem2.transform.position, enem2.transform.rotation);
+        Destroy(enem1);
+        Destroy(enem2);
+        Destroy(exp1.transform.gameObject,1.5f);
+        Destroy(exp2.transform.gameObject,1.5f);
+
+    }
+  
 }
